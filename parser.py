aba32107bde0a0fbed1c4c9e9c93c5c115e8e993
Daniel Monteagudo <dmonte3210@gmail.com>
2016-09-15 17:21:29 -0400

    Switched keys and values to make the random choice easier to code

--- parser.py
+++ parser.py
@@ -1,15 +1,20 @@
+# Opens and reads file into stringthing
 file=open("occupations.csv", "r")
 stringthing = file.read()
+
+# Splits the string into an array called splitString
 splitString = str.split(stringthing, "\r\n")
+
 dict = {}
 
+# Loops through array line by line
 for line in splitString:
     if len(line)>0 and line[0]=='"':
         line = line[1:]
-        dict[line[0:line.index('"')]]=float(line[line.index('"')+2:])
+        dict[float(line[line.index('"')+2:])]=line[0:line.index('"')]
     elif len(line)>0 and splitString.index(line)!=0:
         print line
-        dict[line[0:line.index(',')]]=float(line[line.index(',')+1:])
+        dict[float(line[line.index(',')+1:])]=line[0:line.index(',')]
         
 print dict
 file.close()
