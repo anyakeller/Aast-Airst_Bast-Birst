6176e04e88b4aed8c5928c9e4b37151000b3304a
Daniel Monteagudo <dmonte3210@gmail.com>
2016-09-15 14:05:23 -0400

    made some progress

--- parser.py
+++ parser.py
@@ -1,4 +1,13 @@
-file=open("occupatons.csv", "r")
-s = file.split("\n")
-print(file)
+file=open("occupations.csv", "r")
+stringthing = file.read()
+splitString = str.split(stringthing, "\r\n")
+dict = {}
+
+for line in splitString:
+    if line[0] == '"':
+        line = line[1:]
+        
+        
+
+print(s)
 file.close()
